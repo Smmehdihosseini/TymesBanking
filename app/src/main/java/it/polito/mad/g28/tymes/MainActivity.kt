c82353a6b0e53e32c8a8f3948b32d1100b51ab85
Maximilien Chau <chaumaximilien@gmail.com>
2022-05-24 01:46:15 +0200

    Added Google Auth basis

--- app/src/main/java/it/polito/mad/g28/tymes/MainActivity.kt
+++ app/src/main/java/it/polito/mad/g28/tymes/MainActivity.kt
@@ -1,22 +1,84 @@
 package it.polito.mad.g28.tymes
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.content.IntentSender
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.common.api.ApiException
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var toggle : ActionBarDrawerToggle
     private lateinit var drawerLayout: DrawerLayout
+    private var oneTapClient: SignInClient? = null
+    private var signInRequest: BeginSignInRequest? = null
+    private val REQ_ONE_TAP: Int = 1337
+    private var showOneTapUI = true
+    private lateinit var auth: FirebaseAuth
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Log.d("lifecycle", "oncreate")
+        auth = Firebase.auth
+
+        oneTapClient = Identity.getSignInClient(this);
+        signInRequest = BeginSignInRequest.builder()
+            .setPasswordRequestOptions(
+                BeginSignInRequest.PasswordRequestOptions.builder()
+                .setSupported(true)
+                .build())
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                .setSupported(true)
+                // Your server's client ID, not your Android client ID.
+                .setServerClientId("912277110592-3oo1mgepmkfjmr8qde7cjhe1coo8fvrf.apps.googleusercontent.com")
+                // Only show accounts previously used to sign in.
+                .setFilterByAuthorizedAccounts(false)
+                .build())
+            // Automatically sign in when exactly one credential is retrieved.
+            .setAutoSelectEnabled(true)
+            .build();
+
+        oneTapClient!!.beginSignIn(signInRequest!!)
+            .addOnSuccessListener(this) { result ->
+                try {
+                    Log.d("lifecycle", "success listener")
+                    startIntentSenderForResult(
+                        result.pendingIntent.intentSender, REQ_ONE_TAP,
+                        null, 0, 0, 0, null)
+                } catch (e: IntentSender.SendIntentException) {
+                    Log.e("Sign In", "Couldn't start One Tap UI: ${e.localizedMessage}")
+                }
+            }
+            .addOnFailureListener(this) { e ->
+                // No saved credentials found. Launch the One Tap sign-up flow, or
+                // do nothing and continue presenting the signed-out UI.
+                Log.d("Sign In", e.localizedMessage)
+            }
+
+
 
         drawerLayout = findViewById(R.id.drawerlayout)
         val navView: NavigationView = findViewById(R.id.navigationView)
@@ -27,6 +89,8 @@ class MainActivity : AppCompatActivity() {
         toggle.syncState()
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
 
         navView.setNavigationItemSelectedListener {
 
@@ -49,6 +113,17 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+        //
+    }
+
     private fun changeFrag(fragment: Fragment, title: String){
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
@@ -63,4 +138,54 @@ class MainActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val googleCredential = oneTapClient!!.getSignInCredentialFromIntent(data)
+        val idToken = googleCredential.googleIdToken
+
+        when (requestCode) {
+            REQ_ONE_TAP -> {
+                try {
+                    val credential = oneTapClient!!.getSignInCredentialFromIntent(data)
+                    val idToken = credential.googleIdToken
+                    val username = credential.id
+                    val password = credential.password
+                    when {
+                        idToken != null -> {
+                            val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
+                            auth.signInWithCredential(firebaseCredential)
+                                .addOnCompleteListener(this) { task ->
+                                    if (task.isSuccessful) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d("lifecycle", "signInWithCredential:success")
+                                        val user = auth.currentUser
+                                        updateUI(user)
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.d("lifecycle", "signInWithCredential:failure", task.exception)
+                                        updateUI(null)
+                                    }
+                                }
+
+                            Log.d("Google ID", "Got ID token.")
+                        }
+                        password != null -> {
+                            // Got a saved username and password. Use them to authenticate
+                            // with your backend.
+                            Log.d("Google ID", "Got password.")
+                        }
+                        else -> {
+                            // Shouldn't happen.
+                            Log.d("Google ID", "No ID token or password!")
+                        }
+                    }
+                } catch (e: ApiException) {
+                    // ...
+                }
+            }
+        }
+    }
+
 }
